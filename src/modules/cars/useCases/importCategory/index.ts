18c5725029c4f2078578ff22175121c36146fb28
Emerson Mendonça <emerson_10_s@hotmail.com>
2022-04-12 23:20:07 -0300

    Criando promises para resolver alguns problemas

--- src/modules/cars/useCases/importCategory/index.ts
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -1,7 +1,9 @@
+import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
 import { ImportCategoryController } from "./ImportCategoryController";
 import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
 
-const importCategoryUSeCase = new ImportCategoryUseCase();
+const categoryRepository = CategoriesRepository.getInstance();
+const importCategoryUSeCase = new ImportCategoryUseCase(categoryRepository);
 const importCategoryController = new ImportCategoryController(
   importCategoryUSeCase
 );
