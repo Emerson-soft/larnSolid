1eaf886d5779eb0a05a4a4c8ca66b9e6c49c8166
Emerson Mendonça <emerson_10_s@hotmail.com>
2022-04-30 09:24:39 -0300

    refatorando código

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from "tsyringe";
 
+import { deleteFile } from "../../../../utils/file";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
@@ -17,6 +18,9 @@ class UpdateUserAvatarUseCase {
   async execute({ avatarFile, user_id }: IRequest): Promise<void> {
     const user = await this.userRepository.findById(user_id);
 
+    if (user.avatar) {
+      await deleteFile(`./tmp/avatar/${user.avatar}`);
+    }
     user.avatar = avatarFile;
 
     await this.userRepository.create(user);
