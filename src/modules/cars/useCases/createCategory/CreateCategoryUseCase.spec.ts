724acf533e5b24e7c1e65cfb97e288a8dbdd85e6
Emerson Mendonça <emerson_10_s@hotmail.com>
2022-05-08 00:18:51 -0300

    criado teste para criação de novas categorias de carros

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,8 +1,52 @@
-describe("Create category", () => {
-  it("Espero que 2 + 2 seja 4", () => {
-    const soma = 2 + 2;
-    const result = 4;
+import { AppError } from "../../../../erros/AppError";
+import { CategoriesRepositoryInMemory } from "../../repositories/in-memory/CategoriesRepositoryInMemory";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
-    expect(soma).toBe(result);
+let createCategoryUseCase: CreateCategoryUseCase;
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+
+describe("Create Category", () => {
+  beforeEach(() => {
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+    createCategoryUseCase = new CreateCategoryUseCase(
+      categoriesRepositoryInMemory
+    );
+  });
+
+  it("should be able to create a new category", async () => {
+    const category = {
+      name: "Category test",
+      description: "Category description",
+    };
+
+    await createCategoryUseCase.execute({
+      description: category.description,
+      name: category.name,
+    });
+
+    const categoryCreated = await categoriesRepositoryInMemory.findByName(
+      category.name
+    );
+
+    expect(categoryCreated).toHaveProperty("id");
+  });
+
+  it("should not be able to create a new category with same name", async () => {
+    expect(async () => {
+      const category = {
+        name: "Category test",
+        description: "Category description",
+      };
+
+      await createCategoryUseCase.execute({
+        description: category.description,
+        name: category.name,
+      });
+
+      await createCategoryUseCase.execute({
+        description: category.description,
+        name: category.name,
+      });
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
